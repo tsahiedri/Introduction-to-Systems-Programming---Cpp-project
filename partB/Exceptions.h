1dd2099884816c74afd339a54e2e2611b173f8c2
Tsahi Edri <tsahiedri@campus.technion.ac.il>
2021-11-10 13:22:46 +0200

    basic board game using OOP concepts

--- partB/Exceptions.h
+++ partB/Exceptions.h
@@ -7,7 +7,7 @@ namespace mtm
     class Exception: public std::exception
     {
     public:
-        const char* what() const noexcept override =0;
+        const char* what() const noexcept override = 0;
     };
 
     class  IllegalArgument : public Exception
